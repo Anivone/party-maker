009c639107ba8b3e73a8484addd7127d5c76ef67
Maksym Voloshko <anivone18052001@gmail.com>
2020-12-17 14:35:30 +0200

    [UserAccount entity updated]

--- server/src/models/person/UserAccount.js
+++ server/src/models/person/UserAccount.js
@@ -2,6 +2,8 @@
 const {
     Model
 } = require('sequelize');
+const crypto = require('crypto');
+
 module.exports = (sequelize, DataTypes) => {
     class UserAccount extends Model {
         /**
@@ -16,6 +18,28 @@ module.exports = (sequelize, DataTypes) => {
                 onDelete: 'CASCADE'
             });
         }
+
+        static encryptPassword(password, salt) {
+            return crypto.pbkdf2Sync(
+                password,
+                salt,
+                10000,
+                64,
+                'sha512'
+            ).toString('base64');
+        }
+
+        static isPasswordCorrect(passwordGiven, password, salt) {
+            return crypto.pbkdf2Sync(
+                passwordGiven,
+                salt,
+                10000,
+                64,
+                'sha512'
+                ).toString('base64')
+                === password
+        }
+
     }
 
     UserAccount.init({
@@ -27,6 +51,10 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.STRING,
             allowNull: false,
         },
+        salt: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
     }, {
         sequelize,
         modelName: 'UserAccount',
