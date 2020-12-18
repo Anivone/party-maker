59710733088b42ea52bc791d7a353c9e13f3f66a
Maksym Voloshko <anivone18052001@gmail.com>
2020-12-18 13:20:33 +0200

    [User Service created]

--- server/src/container.js
+++ server/src/container.js
@@ -4,6 +4,10 @@ const passport = require('passport');
 const to = require('await-to');
 const crypto = require('crypto');
 
+// Services
+const UserService = require('./services/user/UserService');
+
+// Repositories
 const PostgresRepository = require('./dal/PostgresRepository');
 
 module.exports = () => {
@@ -14,6 +18,10 @@ module.exports = () => {
         passport: asValue(passport),
         to: asValue(to),
         crypto: asValue(crypto),
+
+
+        // Services
+        userService: asClass(UserService).singleton(),
 
         // Repositories
         postgresRepository: asClass(PostgresRepository).singleton(),
---
+++ server/src/services/user/UserService.js
@@ -0,0 +1,95 @@
+module.exports = class UserService {
+
+    constructor({postgresRepository, to}) {
+        this.postgresRepository = postgresRepository;
+        this.to = to;
+    }
+
+    async findUser(filter, single) {
+        return await single
+            ? this.postgresRepository.findOne('UserAccount', filter)
+            : this.postgresRepository.findAll('UserAccount', filter);
+    }
+
+    async getUser(id) {
+        return await this.postgresRepository.get('UserAccount', id);
+    }
+
+    async createUser(email, password, personId) {
+        const salt = this.postgresRepository.getModel('UserAccount').generateSalt();
+        const pwd = this.postgresRepository.getModel('UserAccount').encryptPassword(password, salt);
+
+        return await this.postgresRepository.create('UserAccount', {email, password: pwd, salt, personId});
+    }
+
+    async updateUser(filter, body) {
+        return await this.postgresRepository.update('UserAccount', filter, body);
+    }
+
+    async removeUser(filter) {
+        return await this.postgresRepository.remove('UserAccount', filter);
+    }
+
+    async Register({
+                       firstName,
+                       lastName,
+                       middleName,
+                       city,
+                       birthDate,
+                       seekingParty,
+                       email,
+                       password,
+                   }) {
+        const to = this.to;
+
+        const [errFind, userFind] = await to(
+            this.findUser({email}, true)
+        );
+
+        if (errFind) throw errFind;
+        if (userFind) throw new Error('User with such email already exists.');
+
+        const [errPerson, person] = await to(
+            this.postgresRepository.create('Person', {
+                firstName,
+                lastName,
+                middleName,
+                birthDate,
+                city,
+                seekingParty
+            })
+        );
+        if (errPerson) throw errPerson;
+
+        const [errUser, user] = await to(
+            this.createUser(email, password, person.id)
+        );
+        if (errUser) throw errUser;
+
+        return user;
+    }
+
+    async Login({
+                    email,
+                    password
+                }) {
+        const to = this.to;
+
+        const [err, user] = await to(
+            this.findUser({email}, true)
+        );
+
+        if (err) throw err;
+        if (!user) throw new Error('User with such email does not exist.');
+
+        if (!this.postgresRepository
+            .getModel('UserAccount')
+            .isPasswordCorrect(password, user.password, user.salt)) {
+
+            throw new Error('Incorrect email or password !');
+        }
+
+        return user;
+    }
+
+}
