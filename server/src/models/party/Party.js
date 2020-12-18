cbfa7e26fd1accda8b38ea32e4288b59df8b2203
Maksym Voloshko <anivone18052001@gmail.com>
2020-12-18 16:58:39 +0200

    [Cleaned up Dependency Injection for mongoose]

--- server/src/models/party/Party.js
+++ server/src/models/party/Party.js
@@ -1,52 +1,50 @@
-const mongoose = require('mongoose');
-
-const partySchema = new mongoose.Schema(
-    {
-        title: {
-            type: mongoose.SchemaTypes.String,
-            required: true,
-        },
-        description: {
-            type: mongoose.SchemaTypes.String,
-            required: true,
-        },
-        address: {
-            city: {
+module.exports = ({mongoose}) => {
+    const partySchema = new mongoose.Schema(
+        {
+            title: {
                 type: mongoose.SchemaTypes.String,
                 required: true,
             },
-            street: {
+            description: {
                 type: mongoose.SchemaTypes.String,
                 required: true,
             },
-            building: {
-                type: mongoose.SchemaTypes.String,
+            address: {
+                city: {
+                    type: mongoose.SchemaTypes.String,
+                    required: true,
+                },
+                street: {
+                    type: mongoose.SchemaTypes.String,
+                    required: true,
+                },
+                building: {
+                    type: mongoose.SchemaTypes.String,
+                    required: true,
+                },
+            },
+            date: {
+                type: mongoose.SchemaTypes.Date,
                 required: true,
             },
-        },
-        date: {
-            type: mongoose.SchemaTypes.Date,
-            required: true,
-        },
-        age: {
-            type: mongoose.SchemaTypes.Number,
-            required: true,
-        },
-        requiredParticipants: {
-            type: mongoose.SchemaTypes.Number,
-            required: true,
-        },
-        maximumParticipants: {
-            type: mongoose.SchemaTypes.Number,
-            required: false,
-        },
-        participants: [{
-            type: mongoose.SchemaTypes.String,
-            required: true,
-        }]
-    }
-);
+            age: {
+                type: mongoose.SchemaTypes.Number,
+                required: true,
+            },
+            requiredParticipants: {
+                type: mongoose.SchemaTypes.Number,
+                required: true,
+            },
+            maximumParticipants: {
+                type: mongoose.SchemaTypes.Number,
+                required: false,
+            },
+            participants: [{
+                type: mongoose.SchemaTypes.String,
+                required: true,
+            }]
+        }
+    );
 
-const Party = mongoose.model('Party', partySchema);
-
-module.exports = Party;
+    return mongoose.model('Party', partySchema);
+}
