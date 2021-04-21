acc6115ce40b2f35f3f71f2e48f72c9802aca198
Maksym Voloshko <anivone18052001@gmail.com>
2021-04-21 15:36:43 +0300

    [Recommendation Service]: Added comments

--- server/src/services/recommendation/RecommendationService.js
+++ server/src/services/recommendation/RecommendationService.js
@@ -21,6 +21,7 @@ module.exports = class RecommendationService {
     }
 
     async getPeopleWithRecommendations() {
+        // Беремо всіх людей
         const [errPerson, people] = await this.to(
             this.personService.findPerson({}, false)
         );
@@ -29,6 +30,7 @@ module.exports = class RecommendationService {
         let result = [];
 
         for (const person of people) {
+            // res = Person із своїми полями + поле recommendedParties
             let res = {
                 ...person.dataValues,
                 recommendedParties: []
@@ -36,14 +38,17 @@ module.exports = class RecommendationService {
             const personId = person.id;
             if (errPerson) throw errPerson;
 
+            // Витягуємо всі паті із повною інформацією
             const [errFind, parties] = await this.to(
                 this.partyService.findPartiesFullInformation({})
             )
             if (errFind) throw errFind;
 
+            // Фільтруємо ті паті, на які ходив юзер
             const personParties = parties
                 .filter(f => f.participants.includes(personId.toString()));
 
+            // Якщо Пьорсон не ходив на паті, то вертаємо перші recommendCount паті для нього
             if (personParties.length === 0) {
                 res.recommendedParties = parties.slice(0, recommendCount);
                 result.push(res);
@@ -64,6 +69,7 @@ module.exports = class RecommendationService {
             };
             let priorityCount = 0;
 
+            // Додає пріоритетність за умови наявності feature y Паті
             personParties.forEach(f => {
                 for (let key in priority) {
                     if (f.features[key]) {
@@ -72,15 +78,19 @@ module.exports = class RecommendationService {
                     }
                 }
             });
+
+            // Обчислюємо значення пріоритетності
             for (let key in priority) {
                 priority[key] = parseFloat((priority[key] / priorityCount / 2)
                     .toFixed(2));
             }
 
+            // Беремо паті, які ми не відвідували
             const otherParties = parties
                 .filter(p => !p.participants.includes(personId.toString()))
                 .sort((a, b) => b.value - a.value);
 
+            // Обчислюємо value (значимість) паті для персони (для нас)
             otherParties.map(p => {
                 p.value = 0;
                 for (let key in priority) {
@@ -95,6 +105,7 @@ module.exports = class RecommendationService {
                 p.value = parseFloat(p.value.toFixed(2));
             });
 
+            // Запихуємо перші recommendCount патіс у recommendedParties персони
             res.recommendedParties = otherParties
                 .sort((a, b) => b.value - a.value)
                 .slice(0, recommendCount);
